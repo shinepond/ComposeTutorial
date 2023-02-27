66be309f278df6bb1a127e22782da8632a59dbd8
shinepond <82484306+shinepond@users.noreply.github.com>
2023-02-27 17:46:20 +0900

    basic components and layout

--- app/src/main/java/com/shinepond/composetutorial/MainActivity.kt
+++ app/src/main/java/com/shinepond/composetutorial/MainActivity.kt
@@ -3,13 +3,19 @@ package com.shinepond.composetutorial
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.shinepond.composetutorial.ui.theme.ComposeTutorialTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +28,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                    drawProfileCard(Message("shinepond","android developer"))
                 }
             }
         }
@@ -30,14 +36,42 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun drawProfileCard(msg: Message) {
+    Row (
+        modifier = Modifier.padding(4.dp)
+    ){
+        Image(
+            painter = painterResource(id = R.drawable.ic_launcher_background),
+            contentDescription = "profile image",
+            modifier = Modifier
+                .size(50.dp)
+                .clip(CircleShape)
+        )
+
+        Spacer(modifier = Modifier.width(8.dp))
+
+        Column (
+            //horizontalAlignment = Alignment.CenterHorizontally
+        verticalArrangement = Arrangement.Center
+                ) {
+            Text(text = msg.author)
+            Spacer(modifier = Modifier.width(4.dp))
+            Text(text = msg.body)
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     ComposeTutorialTheme {
-        Greeting("Android")
+        drawProfileCard(Message("shinepond","android developer"))
     }
+}
+
+class Message(
+    val author : String,
+    val body : String
+) {
+
 }
